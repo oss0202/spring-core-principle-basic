8da6eaedb4afcb0d121c7808dbc8e5de9ac8844a
오성식 <oss0202@naver.com>
2021-10-05 14:35:32 +0900

    Commit log Test

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -18,6 +18,7 @@ public class OrderApp {
         memberService.join(member);
 
         Order order = orderService.createOrder(memberId, "itemA", 10000);
+//        Order order = orderService.createOrder(memberId, "itemA", 10000);
 
         System.out.println("order = " + order);
         System.out.println("order.getDiscountPrice() = " + order.getDiscountPrice());
