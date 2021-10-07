2a4d7db47f8cfeb254299b9e9097ac14e5e74010
오성식 <oss0202@naver.com>
2021-10-07 16:53:41 +0900

    Spring Container Singleton TEst

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,8 +2,12 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,4 +39,37 @@ public class SingletonTest {
         //equal 값비교
     }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤톤")
+    void springContainer(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        //1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println(memberService1);
+        System.out.println(memberService2);
+
+        //참조값이 같은 것을 확인
+        assertThat(memberService1).isSameAs(memberService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤톤 - xml")
+    void springContainerByXml(){
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        //1. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회 : 호출할 때 마다 객체를 생성
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println(memberService1);
+        System.out.println(memberService2);
+
+        //참조값이 같은 것을 확인
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
