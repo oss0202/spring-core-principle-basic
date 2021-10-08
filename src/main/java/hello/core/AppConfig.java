146d82bd79c7d55842ada14de0634ba746db6257
오성식 <oss0202@naver.com>
2021-10-08 09:02:28 +0900

    Configuration 싱글톤

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,44 @@ import org.springframework.context.annotation.Configuration;
 @Configuration// 설정정보 annotation
 public class AppConfig {
 
+    //@Bean memberService -> new MemoryMemberRepository();
+    //@Bean orderService -> new MemoryMemberRepository();
+    // 이 2개의 new MemoryMemberRepository()는 같은가??
+
+    /*
+    1. memberService
+    AppConfig.memberService
+    AppConfig.memberRepository
+
+    2. orderService
+    AppConfig.orderService
+    AppConfig.memberRepository
+
+    3. memberRepository
+    AppConfig.memberRepository
+
+    이 예상 되지만
+
+    AppConfig.memberService
+    AppConfig.memberRepository
+    AppConfig.orderService
+     */
+
     @Bean// Spring container 등록
     public MemberService memberService(){
+        System.out.println("AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService(){
+        System.out.println("AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
